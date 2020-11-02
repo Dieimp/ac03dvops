db27e39bd22d21391b760e1b4e60142a20131de3
Dieimp <mraingue@gmail.com>
2020-11-02 13:40:33 -0300

    arquivos com função primos rodando

---
+++ t8 (1).py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+def nao_entre_em_panico():
+    not_p = []
+    e = []
+    lim = 100
+
+    def primo(num):
+        if num != 1:
+            t = True
+            for i in range(2,num):
+                if num % i == 0:
+                    t = False
+                    break
+            return t
+ 
+    for num in range(2, lim + 1):
+        test = primo(num)
+        if (test):
+            e.append(num)
+        else:
+             not_p.append(num)
+
+    return  e 
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
